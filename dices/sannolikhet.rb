60484f88190202f2bd1bd1b780f1fcc454ca0f48
itgsod-rami-wadala <rami.wadala@elev.it-gymnasiet.se>
2015-03-25 10:43:53 +0100

    om du inte är glad nu så fuck you

--- dices/sannolikhet.rb
+++ dices/sannolikhet.rb
@@ -1,17 +1,7 @@
 require 'colorize'
 
-krpermånad = 1121
-
-månader = 12
-
-år = 10
-
-ränta = 0.0658
-
-puts "a #{krpermånad*(år*månader)}"
-
-puts "b #{krpermånad*ränta}"
-
+puts "  år  mån   betalt  belopp kvar".blue
+puts "______________________________".red
 attbetala = 134520
 i = 0
 betalt = 0
@@ -26,5 +16,40 @@ while i < 120
     f = 1
     d = d + 1
   end
-  puts "år #{d}, månad #{f} och hur mycket du har betalat #{betalt} och du har #{attbetala - betalt} kvar att betala".magenta
+  print " |".red
+  print " #{d} ".magenta
+  print "| ".red
+  print "#{
+        if f <= 9
+          "#{f} ".cyan
+        else
+          "#{f}".blue
+        end} "
+  print "| ".red
+  print " #{
+        if betalt < 10000
+          "#{betalt}  ".red
+        else
+          if betalt < 100000
+            "#{betalt} ".magenta
+          else
+            "#{betalt}".red
+          end
+        end} "
+  print "| ".red
+  print "#{if (attbetala - betalt) < 1000
+  "#{attbetala - betalt}     ".cyan
+  else
+    if (attbetala - betalt) < 10000
+      "#{attbetala - betalt}  ".blue
+    else
+      if (attbetala - betalt) < 100000
+        "#{attbetala - betalt} ".cyan
+      else
+        "#{attbetala - betalt}".blue
+      end
+    end
+  end } "
+  puts "|".red
 end
+puts "––––––––––––––––––––––––––––––".red
